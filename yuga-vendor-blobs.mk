06f1690565993dc29d65cd12b5cfa9d61ee09b92
kamarush <kamarush42251@gmail.com>
2015-10-31 15:22:31 +0100

    update prebuilt modules

--- yuga-vendor-blobs.mk
+++ yuga-vendor-blobs.mk
@@ -169,7 +169,6 @@ PRODUCT_COPY_FILES += \
     vendor/sony/yuga/proprietary/lib/libsys-utils.so:system/lib/libsys-utils.so \
     vendor/sony/yuga/proprietary/lib/libta.so:system/lib/libta.so \
     vendor/sony/yuga/proprietary/lib/modules/radio-iris-transport.ko:system/lib/modules/radio-iris-transport.ko \
-    vendor/sony/yuga/proprietary/lib/modules/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko \
     vendor/sony/yuga/proprietary/lib/modules/wlan.ko:system/lib/modules/wlan.ko \
     vendor/sony/yuga/proprietary/vendor/camera/flash.dat:system/vendor/camera/flash.dat \
     vendor/sony/yuga/proprietary/vendor/camera/KMO08BN0_IMX111.dat:system/vendor/camera/KMO08BN0_IMX111.dat \
